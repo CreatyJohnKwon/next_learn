019d0fc518ba872e15e553a3f7bff90e4a6ec18c
CreatyJohn <vmfodzl1125@naver.com>
2024-12-12 13:00:56 +0900

    comit 1212

--- app/cart/data.tsx
+++ app/cart/data.tsx
@@ -1,15 +1,19 @@
-const cartArr: Array<string> = []
+const cartArr = [
+    { category: "클래식과 트렌드의 완벽한 조화", price: "르퍼스널오더", geather: "오리지널 | 뉴브랜드" },
+    { category: "우아한 일상으로의 초대", price: "메종바인(Maison VINE)", geather: "오리지널 | 뉴브랜드" },
+    { category: "여성의 일상에 스며들다", price: "오떼뜨(Octette)", geather: "오리지널 | 뉴브랜드" }
+]
 
-const CartList = () => {
-    return (
-        <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
-        </div>
-    )
+const countData = 0
+
+export type CartListProp = {
+    category: string,
+    price: string,
+    geather: string,
+    color: string
 }
 
 export {
-    CartList
+    countData,
+    cartArr
 }
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,13 +1,42 @@
 'use client'
 
-import { CartList } from './data'
+import { CartListProp, cartArr } from './data'
+import { useState } from 'react'
 
 export default function Cart() {
     return (
         <div>
             <h4 className="title">Cart</h4>
-            <CartList />
-            <CartList />
+            {
+                cartArr.map((a, i) => {
+                    return (
+                        <CartList
+                            key={i}
+                            category={cartArr[i].category}
+                            price={cartArr[i].price}
+                            geather={cartArr[i].geather}
+                            color={"white"}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+const CartList: React.FC<CartListProp> = ({ category, price, geather, color }) => {
+    let [countNum, setCountNum] = useState(0)
+
+    return (
+        <div className="cart-item">
+            <p>{category}</p>
+            <p>{price}</p>
+            <p>{geather}</p>
+            <button onClick={() => {
+                countNum > 0 ? setCountNum(countNum-1) : alert("0 아래로 내려갈 수 없습니다")
+            }} style={{ backgroundColor : color }}>-</button>
+            <p style={{ color: color }}>{countNum}</p>
+            <button onClick={() => {setCountNum(countNum+1)}} style={{ backgroundColor : color }}>+</button>
         </div>
     )
 }
